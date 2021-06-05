11bdf9c3a570cc61efbd5878304141f00c164853
YumeMichi <do4suki@gmail.com>
2021-06-05 18:17:22 +0800

    sm8250-common: Update blobs from OOS 11.2.5.5
    Signed-off-by: YumeMichi <do4suki@gmail.com>
    Change-Id: I096892cb4f64763b4ecf2b802ed171f092de5d1d

--- common.mk
+++ common.mk
@@ -168,7 +168,8 @@ PRODUCT_COPY_FILES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
-    vendor.qti.hardware.btconfigstore@1.0.vendor
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0.vendor
 
 # Boot control
 PRODUCT_PACKAGES += \
@@ -382,7 +383,9 @@ PRODUCT_BOOT_JARS += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
-    vendor.qti.hardware.perf@2.0.vendor
+    vendor.qti.hardware.perf@2.0.vendor \
+    vendor.qti.hardware.perf@2.1.vendor \
+    vendor.qti.hardware.perf@2.2.vendor
 
 # QMI
 PRODUCT_PACKAGES += \
