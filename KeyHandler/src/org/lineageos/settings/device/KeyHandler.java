b92e8a61294c4c38638aa98b1c32f7f360e2defc
LibXZR <xzr467706992@163.com>
2021-08-19 21:09:11 +0800

    lemonades: Keyhandler: Don't trigger haptic if ringer mode has no change
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>
    Change-Id: Ic7b0241956ebf312d2f3c43f6923a2e8396546eb

--- KeyHandler/src/org/lineageos/settings/device/KeyHandler.java
+++ KeyHandler/src/org/lineageos/settings/device/KeyHandler.java
@@ -49,6 +49,16 @@ public class KeyHandler implements DeviceKeyHandler {
         mVibrator = mContext.getSystemService(Vibrator.class);
     }
 
+    private boolean setRingerModeChecked(int newMode) {
+        int curMode = mAudioManager.getRingerModeInternal();
+
+        if (curMode == newMode)
+            return true;
+
+        mAudioManager.setRingerModeInternal(newMode);
+        return false;
+    }
+
     public KeyEvent handleKeyEvent(KeyEvent event) {
         if (event.getAction() != KeyEvent.ACTION_DOWN) {
             return event;
@@ -58,15 +68,17 @@ public class KeyHandler implements DeviceKeyHandler {
 
         switch (scanCode) {
             case MODE_NORMAL:
-                mAudioManager.setRingerModeInternal(AudioManager.RINGER_MODE_NORMAL);
-                doHapticFeedback(MODE_NORMAL_EFFECT);
+                if (!setRingerModeChecked(AudioManager.RINGER_MODE_NORMAL)) {
+                    doHapticFeedback(MODE_NORMAL_EFFECT);
+                }
                 break;
             case MODE_VIBRATION:
-                mAudioManager.setRingerModeInternal(AudioManager.RINGER_MODE_VIBRATE);
-                doHapticFeedback(MODE_VIBRATION_EFFECT);
+                if (!setRingerModeChecked(AudioManager.RINGER_MODE_VIBRATE)) {
+                    doHapticFeedback(MODE_VIBRATION_EFFECT);
+                }
                 break;
             case MODE_SILENCE:
-                mAudioManager.setRingerModeInternal(AudioManager.RINGER_MODE_SILENT);
+                setRingerModeChecked(AudioManager.RINGER_MODE_SILENT);
                 break;
             default:
                 return event;
