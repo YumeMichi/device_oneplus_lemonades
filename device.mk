e428cd18cd1c339cfe93950404560d3012defa1c
LuK1337 <priv.luk@gmail.com>
2021-08-23 18:00:54 +0200

    lemonades: RIP `remove_packages`
    No longer necessary since now we can just pass `OVERRIDES` to extract
    utils and it'll handle it automatically for us :^)
    
    Change-Id: I19c97f5daa72d809dae2442fcb2b8b81a702aaf2

--- device.mk
+++ device.mk
@@ -414,10 +414,6 @@ PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti \
     libvndfwk_detect_jni.qti.vendor
 
-# Remove unwanted packages
-PRODUCT_PACKAGES += \
-    RemovePackages
-
 # RIL
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full \
