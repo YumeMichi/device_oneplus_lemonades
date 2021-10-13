d9fb94087f2e66a54e88957dc52be1d079815901
Abhishek Aggarwal <warabhishek@gmail.com>
2021-10-13 11:32:48 +0000

    lemonades: Introduce biometrics 2.3 HAL
     * Add 2.3 hal and additional functions
     * Remove lineage fod hal
     * Rename fodextenssion to udfpsextension
     * Remove 2.1 from vintf
     * Adapt to Oneplus Devices by ziasam
    
    Co-authored-by: ziasam <ziauddinsameer@gmail.com>
    Change-Id: If78a8f2ee3a0f83005ffd44087aa469e6de1d913

--- device.mk
+++ device.mk
@@ -293,7 +293,7 @@ PRODUCT_PACKAGES += \
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.oneplus_kona \
-    vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.oneplus_kona \
+    android.hardware.biometrics.fingerprint@2.3-service.oneplus_kona \
     vendor.oneplus.fingerprint.extension@1.0 \
     vendor.oneplus.hardware.display@1.0
 
