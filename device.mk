dfd486941bbc06f4df941a7fd998bd654a04df53
Fabian Leuteneegger <fabian.leutenegger@bluewin.ch>
2021-11-10 21:34:26 +0100

    lemonades: Build custom sensors multihal
    Change-Id: Iedbb9c27028f44d13ff854dbb1ad93c234a7cf96

--- device.mk
+++ device.mk
@@ -261,7 +261,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.0-service.multihal \
+    android.hardware.sensors@2.1-service.multihal.oneplus_kona \
     libsensorndkbridge
 
 # Soong namespaces
