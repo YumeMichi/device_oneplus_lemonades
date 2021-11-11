30a2220570d2f082d05db39259f6621f9620f30b
YumeMichi <do4suki@gmail.com>
2021-11-11 22:43:37 +0800

    lemonades: Update bluetooth blobs from LA.UM.9.12.r1-10800-SMxx50.0
    Signed-off-by: YumeMichi <do4suki@gmail.com>
    Change-Id: Ie274ba7efbf5889fbaa6dc8922165833acbc6620

--- device.mk
+++ device.mk
@@ -124,9 +124,11 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth.audio@2.0-impl \
     audio.bluetooth.default \
-    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    android.hardware.bluetooth.audio@2.0-impl \
+    com.qualcomm.qti.bluetooth_audio@1.0.vendor \
+    libbluetooth_audio_session \
+    vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
