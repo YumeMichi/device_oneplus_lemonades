055d2c35387e12d096ece4b11dccc2edad92de4d
YumeMichi <do4suki@gmail.com>
2021-11-21 14:08:52 +0800

    lemonades: Switch to Liyuu Clang 14.0.0
    Signed-off-by: YumeMichi <do4suki@gmail.com>
    Change-Id: Icc96f135877f184b15cfc999006857d017f215a5

--- device.mk
+++ device.mk
@@ -14,6 +14,9 @@
 # limitations under the License.
 #
 
+# Clang
+PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := false
+
 # Enable virtual A/B OTA
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
